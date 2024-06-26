f9949a698a0e82869fc446d2c5bf73ef914d074b
Bagavathi2002 <140944606+Bagavathi2002@users.noreply.github.com>
2024-06-26 20:44:42 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c",&ch);
+    switch(ch){
+        case 'A'...'Z':
+        printf("Character is Uppercase");
+        break;
+        case 'a'...'z':
+        printf("Character is Lowercase");
+        break;
+        case '0'...'9':
+        printf("Character is a digit");
+        break;
+        default:
+        printf("Not an alphabet or digit");
+        
+    }
+}
