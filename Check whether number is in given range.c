f8a4399aee97c87cc89dd0b2bc9a05c7667a21d1
Bagavathi2002 <140944606+Bagavathi2002@users.noreply.github.com>
2024-06-27 12:31:38 +0530

    Add files via upload

---
+++ Check whether number is in given range.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    if(a>=50 && a<=100){
+        printf("%d is in range",a);
+    }
+    else{
+        printf("%d is not in range",a);
+    }
+}
